ab91ae5d83c37d6e6dc770f165307d02dcdae6f4
Edward-Xh-svg <vladimirachraf3@gmail.com>
2026-05-05 18:36:06 +0100

    Remove comment and adjust GEMINI_API_KEY assignment

--- config.py
+++ config.py
@@ -3,6 +3,4 @@
 BOT_TOKEN = "8259423389:AAER55cyS_QeV3o9yC4xJKkmZc3RHEvXm44"
 ADMIN_ID = "@edwardmaystro"
 GROUP_LINK = "https://t.me/+IfVjK5l6_2M1MGI0"
-
-# مفتاح Gemini الخاص بك
 GEMINI_API_KEY = "AIzaSyD_lnv5puPif9dJngU5WaKBa5VVNk_qNk4"
